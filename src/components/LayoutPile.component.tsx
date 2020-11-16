7873c7b219880475ccfc7aeaf20230d142803d71
Emma Tilden <e.k.tilden@gmail.com>
2020-11-15 18:23:32 -0600

    update

--- src/components/LayoutPile.component.tsx
+++ src/components/LayoutPile.component.tsx
@@ -1,23 +1,31 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { ICard } from '../models/Card'
 
 const LayoutPile = ({pileCards}: {pileCards: ICard[]}) => {
+  let initialLayoutPileCards: ICard[] = []
+  let [layoutPileCards, setLayoutPileCards] = useState(initialLayoutPileCards)
 
+  useEffect(() => {
+    layoutPileCards = pileCards
+    flipCard()
+  }, pileCards)
 
-  const flipCard = (card: ICard):ICard => {
-    card.faceUp = true
-    return card
+  const flipCard = (): void => {
+    if (layoutPileCards.length) {
+      let flipped = layoutPileCards[0]
+      flipped.faceUp = true
+      setLayoutPileCards([flipped, ...layoutPileCards.slice(1)])
+    }
   }
 
   return (
     <div>
-      {pileCards.length ? pileCards.map((card,i) => {
-        if (i===0) {
-          flipCard(card)
-          return (<div key={i+'layout'} className="card layoutCard">{`${card.getDisplayValue()} of ${card.suit}s`}</div>)
-        }
-        //TODO: facedown pile stack border or something idk
-        //return <div className="faceDownCard layoutFaceDownStack"></div>
+      {pileCards.length ? pileCards.map(card => {
+          if(card.faceUp) {
+            return <div key={card.cardValue + card.suit} className="card layoutCard">{`${card.getDisplayValue()} of ${card.suit}s`}</div>
+          } else {
+            return <div key={card.cardValue + card.suit} className="card faceDownCard"></div>
+          }
       }): <div className="emptyCardPile">Empty Pile</div>}
     </div>
   );
